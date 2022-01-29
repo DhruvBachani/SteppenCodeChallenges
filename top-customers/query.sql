5078bdc972f6b65ab0ff4293e0831212a0996086
Dhruv <s3808392@student.rmit.edu.au>
2022-01-29 15:15:09 +1100

    converted query to Postgresql

--- top-customers/query.sql
+++ top-customers/query.sql
@@ -1,22 +1,26 @@
 select
-    strftime('%Y', a.ordered_at) as year,
-    strftime('%m', a.ordered_at) as Month,
-    a.customer_id,
-    max(total_order_value)
+    a.year,
+    a.month,
+    min(a.customer_id) as customer_id,
+    max(a.total_order_value) as total_monthly_order_value
 from
     (
         select
             orders.customer_id,
-            orders.ordered_at,
-            orders.order_id,
+            date_part('month', orders.ordered_at) as month,
+            date_part('year', orders.ordered_at) as year,
             sum(
-                order_line_items.quantity * order_line_items.unit_price
+                order_line_items.unit_price * order_line_items.quantity
             ) as total_order_value
         from
             orders
             join order_line_items on orders.order_id = order_line_items.order_id
         group by
+            orders.customer_id,
             orders.order_id
+        order by
+            month
     ) a
 group by
-    strftime('%m-%Y', a.ordered_at)
+    year,
+    month
